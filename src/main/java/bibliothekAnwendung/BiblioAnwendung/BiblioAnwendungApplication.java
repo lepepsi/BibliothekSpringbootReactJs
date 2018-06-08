e89c1a911f86cc49c757d28bbfcc50306749de9a
stephane ngameni <Stephane.Ngameni@hermesworld.com>
2018-06-08 19:55:21 +0200

    Initial Commit

---
+++ src/main/java/bibliothekAnwendung/BiblioAnwendung/BiblioAnwendungApplication.java
@@ -0,0 +1,12 @@
+package bibliothekAnwendung.BiblioAnwendung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioAnwendungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioAnwendungApplication.class, args);
+	}
+}
